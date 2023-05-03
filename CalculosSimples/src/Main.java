b659d999b85e00299434e0d5a1b4320e0ebc2ce8
CristianoRodrigues12 <kriks.red@gmail.com>
2023-05-03 18:32:59 -0300

    Exercico de calculos simples com resultado

---
+++ CalculosSimples/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exercício de cálculos!!");
+
+        Calculador.soma(5, 8);
+        Calculador.subtracao(10, 7.5);
+        Calculador.multiplicacao(5, 8);
+        Calculador.divisao(25, 5);
+    }
+}
